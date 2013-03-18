c12a183405f9dcc8ea0ed5ff3e2dc4db77f86d89
jucoba <jubel.correa@gmail.com>
2013-03-17 19:39:21 -0500

    Added required validation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :quantity, :value
+
+	validates :name, :presence => true
+	validates :value, :presence => true
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,36 @@ require 'spec_helper'
 
 describe Product do
 	
-	subject { @product }
-
-	before ( :each ) do
-		@attr = {name: "Dish Washer", quantity: 4, value:  20000} 
-		@product = Product.new(@attr)
-	end
-
-	it { should respond_to(:name) }	
-	it { should respond_to(:quantity) }	
-	it { should respond_to(:value) }	
 	
 	describe "Create products" do
 	
 
+		subject { @product }
+
+		before ( :each ) do
+			@attr = {name: "Dish Washer", quantity: 4, value:  20000} 
+				@product = Product.new(@attr)
+			end
+
+		it { should respond_to(:name) }	
+		it { should respond_to(:quantity) }	
+		it { should respond_to(:value) }	
+
 		it "Must create a dish washer that cost 20000 with 4 units on stock" do	
 			@product.save
 			@product.quantity.should == 4
+			@product.value.should == 20000
+			@product.name.should == "Dish Washer"
+		end
+
+		it "Must have a name" do
+			no_name_product = Product.new( @attr.merge(:name => "") )
+			no_name_product.should_not be_valid
+		end
+		
+		it "Must have a value" do
+			no_value_product = Product.new( @attr.merge(:value => "") )
+			no_value_product.should_not be_valid
 		end
 	end	
 end
